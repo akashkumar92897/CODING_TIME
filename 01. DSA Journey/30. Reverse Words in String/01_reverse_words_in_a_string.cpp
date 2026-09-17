43c9f57713dd157cb15f3c2f92236579a8c2e333
Akash Kumar <akashkumar92897@gmail.com>
2026-09-17 11:38:39 +0530

    feat(strings): reverse words in a string

---
+++ 01. DSA Journey/30. Reverse Words in String/01_reverse_words_in_a_string.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string reverseWords(string s){
+    string ans = "";
+    reverse(s.begin(), s.end());
+
+    for (int i=0; i<s.length(); i++){
+        string word = "";
+        while (i < s.length() && s[i] != ' '){
+            word += s[i];
+            i++;
+        }
+        reverse(word.begin(), word.end());
+        if(word.length() > 0){
+            ans += " " + word;
+        }
+    }
+    return ans.substr(1);
+}
+
+int main()
+{
+    string s1 = "the sky is blue";
+    cout << reverseWords(s1);
+    return 0;
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(n)
